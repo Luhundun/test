ab7f17f01a453530258cb62d8f7d7b3f9e84604c
Luhundun <57153251@qq.com>
2023-08-19 21:20:56 +0800

    建立git仓库--by 卢宁

---
+++ src/nuaa/luning/phone/GetPhoneTest.java
@@ -0,0 +1,14 @@
+package nuaa.luning.phone;
+
+import org.junit.jupiter.api.Test;
+
+public class GetPhoneTest {
+    @Test
+    public void getPhoneInfoTest(){
+        //创建一个MobileCodeWSSoap对象
+        MobileCodeWSSoap mobileCodeWSSoap = new MobileCodeWS().getMobileCodeWSSoap();
+        //调用WebService提供的getMobileCodeInfo方法查询手机号码的归属地
+        String searchResult = mobileCodeWSSoap.getMobileCodeInfo("13372018629", null);
+        System.out.println(searchResult);
+    }
+}
